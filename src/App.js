f3e0a3e41caaf33999b2993976a75f41cdfb0160
dimablbn <dimka.blbn@gmail.com>
2021-07-27 00:05:07 +0300

    sell car

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SellCar from './components/SellCar/SellCar';
+
+function App() {
+    return (
+        <div>
+            <SellCar/>
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/SellCar/SellCar.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import Select from 'react-select'
+import { SelectWrapper } from './styles.js'
+
+
+const optionsMaker = [
+    { value: 'Maker', label: 'Maker' },
+    { value: 'Maker-1', label: 'Maker-1' },
+    { value: 'Maker-2', label: 'Maker-2' }
+  ]
+  const optionsModel = [
+    { value: 'Model', label: 'Model' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+  const optionsYear = [
+    { value: 'Year', label: 'Year' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+  const optionsTrim = [
+    { value: 'Trim', label: 'Trim' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
+function SellCar() {
+    return (
+        <SelectWrapper>
+            <div className="select">
+                <div className="select__wrapper">
+                    <Select className="select__item" options={optionsModel} defaultValue={optionsModel[0]}/>
+                    <Select className="select__item" options={optionsYear} defaultValue={optionsYear[0]}/>
+                    <input className=" select__label" placeholder="KM" type="text" />
+                     
+                </div>
+                <div className="select__wrapper">
+                    <Select className="select__item" options={optionsTrim} defaultValue={optionsTrim[0]}/>
+                    <Select className="select__item" options={optionsMaker} defaultValue={optionsMaker[0]}/>
+                    <input className=" select__label" placeholder="Price" type="text" />
+                </div>
+                <textarea className="select__label select__descripteon" cols="30" rows="10" placeholder="Description"></textarea>
+            </div>
+            <div className="select-surce">
+                <img src='https://lh3.google.com/u/1/d/1JhMN7mPibD8j-vgLyj6A87ofKiPLdK-G=w1920-h969-iv1' alt="" />
+            </div>
+        </SelectWrapper>
+        
+    )
+}
+
+export default SellCar
