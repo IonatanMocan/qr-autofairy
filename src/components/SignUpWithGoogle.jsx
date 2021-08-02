3988632ebeb49368e0fc9c658f8688c1cef61c81
dimablbn <dimka.blbn@gmail.com>
2021-08-03 02:36:45 +0300

    add button att google

--- src/components/SignUpWithGoogle.jsx
+++ src/components/SignUpWithGoogle.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components';
-import SignUpWithGoogleICon from '../images/SignUpWithGoogle.png';
+import GoogleLogin from 'react-google-login';
 const GoogleButtonWrapper = styled.div`
     width: 100%;
     justify-content: center;
     display: flex;
     margin: 40px 0;
 `
-  function SignUpWithGoogle() {
+const responseGoogle = (response) => {
+  console.log(response);
+}
+class SignUpWithGoogle extends Component {
+    render() {
       return (
-        <GoogleButtonWrapper>
-            <img src={SignUpWithGoogleICon} alt="" />
-        </GoogleButtonWrapper>
-  );
+            <GoogleLogin
+            clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+          />
+      )
+    }
 }
   
   export default SignUpWithGoogle
